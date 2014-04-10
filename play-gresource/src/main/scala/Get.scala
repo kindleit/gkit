f980bd8587b4b91e6cdc94042617663d3fa509fb
Carlos Encarnacion <encarnacion.carlos@gmail.com>
2014-04-10 11:21:33 -0400

    Improve Get operation

--- play-gresource/src/main/scala/Get.scala
+++ play-gresource/src/main/scala/Get.scala
@@ -6,11 +6,32 @@ import play.api.mvc._
 import play.core.Router._
 import play.core._
 
-case class Get[A](mkResult: (Request[AnyContent], A) => Result)
-  (implicit pc: ParamsCollector[A]) extends Op {
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+
+class Get[A, B](bp: BodyParser[A])(run: Request[A] => B => Future[SimpleResult])
+  (implicit ec: ExecutionContext, pc: ParamsCollector[B]) extends Op {
 
   lazy val route = Route("GET", PathPattern(List(StaticPart(prefix))))
 
   def mkResponse(params: RouteParams) =
-    pc.collect(params).fold(e => Action(BadRequest(e)), v => Action(req => mkResult(req, v)))
+    pc.collect(params).fold(
+      e => Action.async(bp)(_ => Future(BadRequest(e))),
+      p => Action.async(bp)(run(_)(p)))
+}
+
+object Get {
+  def apply(run: Request[AnyContent] => Future[SimpleResult])
+    (implicit ec: ExecutionContext) = {
+    implicitly[ParamsCollector[Unit]]
+    new Get[AnyContent, Unit](BodyParsers.parse.anyContent)(r => _ => run(r))
+  }
+
+  def get[A](run: Request[AnyContent] => A => Future[SimpleResult])
+    (implicit ec: ExecutionContext, pc: ParamsCollector[A]) =
+    new Get[AnyContent, A](BodyParsers.parse.anyContent)(run)
+
+  def withParser[A, B](bp: BodyParser[A])(run: Request[A] => B => Future[SimpleResult])
+    (implicit ec: ExecutionContext , pc: ParamsCollector[B]) =
+    new Get[A, B](bp)(run)
 }
