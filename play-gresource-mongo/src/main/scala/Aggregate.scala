57335d79b46efa52c8ad3adb41f5cac88fa58af2
Carlos Encarnacion <encarnacion.carlos@gmail.com>
2014-04-24 19:24:18 -0400

    Improve aggregate operation

--- play-gresource-mongo/src/main/scala/Aggregate.scala
+++ play-gresource-mongo/src/main/scala/Aggregate.scala
@@ -13,15 +13,21 @@ import play.core._
 import scala.concurrent.Future
 
 import scalaz._
+import Scalaz._
 
 import shapeless._
 
-class Aggregate[A, B <: HList](cname: String, pipeline: B)
+class Aggregate[A, B, C <: HList]
+  (
+    cname: String
+  , mkPipeline: Request[AnyContent] => B => Future[String \/ C]
+  )
   (implicit
     dbe: DbEnv
-  , rbp: BSONPickler[A]
-  , qbp: BSONPickler[B]
+  , bp1: BSONPickler[A]
+  , bp3: BSONPickler[C]
   , jsp: JSONPickler[A]
+  , pc: ParamsCollector[B]
   ) extends Op[AnyContent] {
 
   import play.modules.gjson.JSON._
@@ -32,29 +38,38 @@ class Aggregate[A, B <: HList](cname: String, pipeline: B)
 
   def action(rp: RouteParams) = {
 
-    def buildResult(r: Request[AnyContent]) = {
-      val f = collection(cname).aggregate[A](pipeline)
-      f.map(_.fold(InternalServerError(_), as => Ok(toJSON(as))))
-    }
+    def toStatus(r: String \/ A) =
+      r.fold(InternalServerError(_), as => Ok(toJSON(as)))
+
+    def aggregate(r: Request[AnyContent])(ps: B) =
+      mkPipeline(r)(ps).flatMap(_.fold(e => Future(e.left), x => collection(cname).aggregate[A](x)))
+
+    def buildResult(r: Request[AnyContent]) =
+      pc.collect(rp).fold(e => Future(BadRequest(e)), ps => aggregate(r)(ps).map(toStatus))
 
     buildAction(parse.anyContent)(buildResult)
   }
 
   def filter(f: Request[AnyContent] => Future[Boolean]) =
-    new Aggregate[A, B](cname, pipeline) {
+    new Aggregate[A, B, C](cname, mkPipeline) {
       override def accept(r: Request[AnyContent]) = f(r)
     }
 }
 
 object Aggregate {
   def apply[A] = new {
-    def apply[B <: HList](cname: String, pipeline: B)
-    (implicit
-      dbe: DbEnv
-    , rbp: BSONPickler[A]
-    , bsp: BSONPickler[B]
-    , jsp: JSONPickler[A]
-    )
-    = new Aggregate[A, B](cname, pipeline)
+    def apply[B, C <: HList]
+      (
+        cname: String
+      , mkPipeline: Request[AnyContent] => B => Future[String \/ C]
+      )
+      (implicit
+        dbe: DbEnv
+      , rbp: BSONPickler[A]
+      , bsp: BSONPickler[C]
+      , jsp: JSONPickler[A]
+      , pc: ParamsCollector[B]
+      )
+      = new Aggregate[A, B, C](cname, mkPipeline)
   }
 }
